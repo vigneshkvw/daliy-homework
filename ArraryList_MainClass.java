1dcc46c190eb743f5b16734e9f74ef77b25bfc84
vigneshkvw <vignesh.vwk@gmail.com>
2017-07-28 13:46:31 +0530

    Add files via upload

---
+++ ArraryList_MainClass.java
@@ -0,0 +1,30 @@
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author viki
+ */
+
+public class ArraryList_MainClass {
+
+	public static void main(String args[]) {
+
+		ArraryList_1 obj = new ArraryList_1();
+
+		obj.getVal();
+		obj.display();
+        obj.insert();
+        obj.sublist();
+        obj.deleteindex();
+        obj.deleteArray();
+       
+
+    }
+
+}
